062325e88add725228ef1a9db0cb9d6480cae333
alexrozanov <alexrozans@gmail.com>
2022-11-21 11:38:33 +0300

    add new folder

--- Seminar1/Zadanie2/Program.cs
+++ Seminar1/Zadanie2/Program.cs
@@ -3,3 +3,7 @@ Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+    Console.Write("yes");
+else
+    Console.Write("no");
