768360b75658fb3bcf8bcfd524477ab602c950b6
alexrozanov <alexrozans@gmail.com>
2022-11-21 11:06:32 +0300

    initial commit

---
+++ Seminar1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); //2147000000
+int n = int.Parse(Console.ReadLine()); //Ввод чисел
+Console.Write(n > 5);
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);
