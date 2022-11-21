02c6e23f5503469210e2824155c5b994f159aa52
alexrozanov <alexrozans@gmail.com>
2022-11-21 22:09:31 +0300

    edit file, add info

--- Seminar1/Zadanie5_for/Program.cs
+++ Seminar1/Zadanie5_for/Program.cs
@@ -4,13 +4,9 @@
 // N до N.
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 
-// Через while
+// Через for
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
-int i = n * (-1);
-while (i <= n)
-{
-    Console.Write($"{i} ");
-    i = i + 1; // i++;
-}
+for (int i = (-1) * n; i <= n; i++)
+    Console.Write(i + "  ");
