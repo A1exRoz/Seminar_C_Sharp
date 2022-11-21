4becf42ec1e1a80699a690d390e124c08bad1877
alexrozanov <alexrozans@gmail.com>
2022-11-21 11:20:58 +0300

    add new folder and change dir

--- Seminar1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.Clear();
-Console.Write("Введите первое число: ");
-int n = Convert.ToInt32(Console.ReadLine()); //2147000000
-int n = int.Parse(Console.ReadLine()); //Ввод чисел
-Console.Write(n > 5);
---
+++ Seminar1/Zadanie2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
