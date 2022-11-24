72c050245c7493270c9d0c78dd4e22cca91e3da2
alexrozanov <alexrozans@gmail.com>
2022-11-24 11:40:11 +0300

    add new task

---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+    Console.Write("yes");
+else
+    Console.Write("no");
---
+++ Seminar2/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая
+// принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b || b * b == a)
+    Console.Write("yes");
+else
+    Console.Write("no");
