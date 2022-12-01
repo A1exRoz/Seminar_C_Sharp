32d4cea5b09efc20dc50c95b75904b115ac4aed1
alexrozanov <alexrozans@gmail.com>
2022-12-01 11:59:12 +0300

    new task after seminar

---
+++ Seminar4/Procedura/Program.cs
@@ -0,0 +1,10 @@
+﻿void maxNumbers(ref int a) // процедура
+{
+    a = a + 10;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+maxNumbers(ref n);
+Console.WriteLine($"Результат: {n}");
---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 24. Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму
+// чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Результат: {(n + 1) / 2 * n}");
+
+
